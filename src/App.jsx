3729a92e142d5ecc56d4ad0e43b28a85446be884
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-28 18:03:53 +0100

    add upload button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { storage } from '../firebase'
+import { ref, uploadBytes } from "firebase/storage"
+import { v4 } from 'uuid'
 
 function App() {
  
+  const [fileUpload, setFileUpload] = useState(null)
+  const uploadFile = () => {
+    if(uploadFile == null) return;
+    const imageRef = ref(storage, `files/${fileUpload.name + v4()}`)
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded")
+    })
+  }
+
   return (
     <>
-      <div>
-          Hi there!
+      <div className='app'>
+        Hi there
+        <input type="file" onChange={(event) => {setFileUpload(event.target.files[0])}}/>
+          <button onClick={fileUpload}>Upload</button>
 
       </div>
     </>
