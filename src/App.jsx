a1565abec456d1053ecd00372c4b0281128ed117
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-30 14:01:33 +0100

    add state for retrieving files from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { storage } from '../firebase'
-import { ref, uploadBytes } from "firebase/storage"
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage"
 import { v4 } from 'uuid'
 
 function App() {
  
   const [fileUpload, setFileUpload] = useState(null)
+  const [fileList, setFileList] =useState([])
+  const fileListRef = ref(storage, "files/")
+
   const uploadFile = () => {
     if(uploadFile == null) return;
     const fileRef = ref(storage, `files/${fileUpload.name + v4()}`)
@@ -15,6 +18,16 @@ function App() {
     })
   }
 
+  useEffect(() => {
+    listAll(fileListRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setFileList((prev) => [...prev, url])
+        })
+      })
+    })
+  }, [])
+
   return (
     <>
       <div className='app'>
@@ -22,6 +35,9 @@ function App() {
         <input type="file" onChange={(event) => {setFileUpload(event.target.files[0])}}/>
           <button onClick={uploadFile}>Upload</button>
 
+          {/* {fileList.map((url) => {
+            return <img src={url}></img>
+          })} */}
       </div>
     </>
   )
